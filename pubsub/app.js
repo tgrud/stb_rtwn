a765a40a95d2ef80c3e90a23e75684691fcbce9c
tgrud <tgrud@users.noreply.github.com>
2015-09-09 02:26:51 -0500

    Create start of badge posting functionality. POST request URL will stores badges in redis. Minor code adjustments.

--- pubsub/app.js
+++ pubsub/app.js
@@ -2,11 +2,12 @@
 
 var express = require('express');
 var app = express();
+var badges = require('./controllers/badges');
 
 app.use(express.json());
 
-app.post('/', function(req, res) {
-  res.send('hello world');
+app.post('/', badges.save, function(req, res) {
+  res.send('\ndone\n\n');
 });
 
-app.listen(8000);
+app.listen(8000, "0.0.0.0");
---
+++ pubsub/controllers/badges.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var _ = require('underscore');
+var badge = require('../models/badges');
+
+/**
+ *  Send badges to model to be saved
+ */
+exports.save = function(req, res, next) {
+  var badges = _.clone(req.body);
+  badge.save(badges, function(err) {
+    if(err) return res.json(503, { error: true });
+    next();
+  });
+};
+
+/**
+ *  Send badges to pub/sub socket in model
+ */
+exports.send = function() {};
